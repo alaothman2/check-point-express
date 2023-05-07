f52378b81876d55efef52e260d58ead723981212
alaa <alatothman110@gmail.com>
2023-05-07 14:08:55 +0100

    check point

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("view-engine", "ejs");
+app.use(express.static(path.join(__dirname,'public')))
+
+
+app.get("/home", (req, res) => {
+  res.render("home.ejs");
+});
+app.get("/contact-us", (req, res) => {
+  res.render("Contact-us.ejs");
+});
+
+app.get("/our-services", (req, res) => {
+  res.render("Our-services.ejs");
+});
+
+
+
+app.listen(4000);
